01bec899cc256ec261cd1fa409f0caf538865d18
supersaiyan486 <853075176@qq.com>
2026-02-03 02:51:36 +0800

    change upload

--- myres/a.c
+++ myres/a.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
+
 	printf("1testing...\n");
 	printf("2testing...\n");
 	printf("3testing...\n");
 	printf("4testing...\n");
 	printf("5testing...\n");
-	printf("6testing...\n")
+	printf("6testing...\n");
 
-	
-
+	return 0;
 
 
 
